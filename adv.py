ad2f93f26bac691d33c0cce0348e6c207f469a4c
maxim371 <kwaku.dr@gmail.com>
2020-11-23 15:20:22 -0500

    Graph Sprint

--- adv.py
+++ adv.py
@@ -28,6 +28,42 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+track_path = []
+
+#function that returns reversed direction
+def reverse(dir):
+    if dir == 'n':
+        return 's'
+    if dir == 's':
+        return 'n'
+    if dir == 'w':
+        return 'e'
+    if dir == 'e':
+        return 'w'
+    else:
+        return 'error'
+
+# keep track of rooms visited 
+rooms = {}
+# first room visted with exits
+rooms[player.current_room.id] = player.current_room.get_exits()
+while len(rooms) < len(room_graph) - 1:
+    if player.current_room.id not in rooms:
+        rooms[player.current_room.id] = player.current_room.get_exits()
+        last_room = track_path[-1]
+        rooms[player.current_room.id].remove(last_room)
+    #if the rooms finished being explored
+    while len(rooms[player.current_room.id]) < 1:
+        track = track_path.pop()
+        player.travel(track)
+        traversal_path.append(track)
+    #if there are other rooms to visit
+    else:
+        last_visit = rooms[player.current_room.id].pop()
+        traversal_path.append(last_visit)
+        track_path.append(reverse(last_visit))
+        player.travel(last_visit)
+
 
 
 
